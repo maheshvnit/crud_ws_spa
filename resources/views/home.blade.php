7ff0e312a3e1d9d44ebf04b998f72c9e68d51d95
Mahesh Prasad <mahesh.vrce@gmail.com>
2019-07-27 13:01:42 +0530

    home and auth controllers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,6 +19,11 @@
                         CRUD SPA
                     </div>
 
+                    <div>
+                        <passport-clients></passport-clients>
+                        <passport-authorized-clients></passport-authorized-clients>
+                        <passport-personal-access-tokens></passport-personal-access-tokens>                    
+                    </div>
 
                 </div>
             </div>
