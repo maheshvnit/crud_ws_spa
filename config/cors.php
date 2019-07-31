c82a51d5f46de461e151db6e4d4d6294fbe3e0da
Mahesh Prasad <mahesh.vrce@gmail.com>
2019-07-31 16:18:22 +0530

    Adds CORS (Cross-Origin Resource Sharing) headers support in your Laravel application https://github.com/barryvdh/laravel-cors

---
+++ config/cors.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With'],
+
+    'allowedHeaders' => ['Accept', 'Content-Type', 'Authorization', 'X-Requested-With', 'X-Initialized-At'],
+
+    */
+   
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
